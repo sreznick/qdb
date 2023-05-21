26c2d3448f4ddf3bece9572344e751ae2010830c
Jarlid <korkiniostudymail@gmail.com>
2023-05-21 16:51:00 +0300

    Interactive mode done

--- src/parser.y
+++ src/parser.y
@@ -14,7 +14,9 @@
     }
 }
 
-%token PLUS MINUS TIMES LPAREN RPAREN
+%token ASSIGN
+%token KW_EQ KW_NEQ
+%token PLUS MINUS TIMES DIVIDE LPAREN RPAREN
 %token SEMICOLON
 %token COMMA
 %token KW_CREATE
@@ -26,42 +28,93 @@
 %token KW_DELETE
 %token KW_UPDATE
 %token KW_CHAR
+%token KW_VARCHAR
+%token KW_INT
+%token KW_REAL
+%token KW_TEXT
+%token KW_BOOLEAN
+%token KW_TRUE
+%token KW_FALSE
+%token KW_OR
+%token KW_AND
+%token KW_NOT
+%token KW_VALUES
+%token KW_WHERE
+%token KW_SET
 
+%nonassoc ASSIGN
+%left KW_OR
+%left KW_AND
+%nonassoc KW_NOT
+%left KW_EQ KW_NEQ
 %left PLUS MINUS
-%left TIMES
+%left TIMES DIVIDE
 
 %union {
   std::string *name;
   int number;
+  float fnumber;
+  std::string *svalue;
+
   query::Query* query;
   query::DataType* type;
-  std::vector<query::FieldDefinition*>* fieldDefs;
+  std::vector<query::FieldDefinition*>* field_defs;
+
+  query::FieldValue* value;
+  std::vector<query::FieldValue*>* field_values;
+
+  query::LogicalExpr* logical_expr;
+  query::Expr* expr;
+  std::vector<query::Expr*>* exprs;
+
+  query::FieldSet* field_set;
+  std::vector<query::FieldSet*>* field_sets;
 }
 
 %token <name> IDENTIFIER
 %token <number> NUMBER
-%nterm <number> expr
+%token <fnumber> REAL
+%token <svalue> STRING
 %nterm <query> start
 %nterm <type> type
-%nterm <fieldDefs> field_defs
+%nterm <field_defs> field_defs
+%nterm <value> value
+%nterm <field_values> field_values
+%nterm <logical_expr> logical_expr
+%nterm <expr> expr
+%nterm <exprs> exprs
+%nterm <field_set> field_set
+%nterm <field_sets> field_sets
 %%
 
 
 start: 
-    KW_CREATE KW_TABLE IDENTIFIER LPAREN field_defs RPAREN SEMICOLON { 
+    KW_CREATE KW_TABLE IDENTIFIER LPAREN field_defs RPAREN SEMICOLON {
         query::CreateTable* createTable = new query::CreateTable(*$3, $5);
         query::Query* query = new query::Query(createTable);
         $$ = query;
         *ret = query;
     } |
-    KW_INSERT KW_INTO IDENTIFIER SEMICOLON {
-        query::Insert* insert = new query::Insert(*$3);
+    KW_INSERT KW_INTO IDENTIFIER KW_VALUES LPAREN field_values RPAREN SEMICOLON {
+        query::Insert* insert = new query::Insert(*$3, $6);
         query::Query* query = new query::Query(insert);
         $$ = query;
         *ret = query;
     } |
-    KW_SELECT KW_FROM IDENTIFIER SEMICOLON {
-        query::Insert* insert = new query::Insert(*$3);
+    KW_SELECT TIMES KW_FROM IDENTIFIER SEMICOLON {
+        query::Select* insert = new query::Select(*$4);
+        query::Query* query = new query::Query(insert);
+        $$ = query;
+        *ret = query;
+    } |
+    KW_SELECT TIMES KW_FROM IDENTIFIER KW_WHERE logical_expr SEMICOLON {
+        query::Select* insert = new query::Select(*$4, $6);
+        query::Query* query = new query::Query(insert);
+        $$ = query;
+        *ret = query;
+    } |
+    KW_SELECT exprs KW_FROM IDENTIFIER KW_WHERE logical_expr SEMICOLON {
+        query::Select* insert = new query::Select(*$4, $6, $2);
         query::Query* query = new query::Query(insert);
         $$ = query;
         *ret = query;
@@ -72,8 +125,14 @@ start:
         $$ = query;
         *ret = query;
     } |
-    KW_UPDATE IDENTIFIER SEMICOLON {
-        query::Update* update = new query::Update(*$2);
+    KW_DELETE KW_FROM IDENTIFIER KW_WHERE logical_expr SEMICOLON {
+            query::Delete* deleteQuery = new query::Delete(*$3, $5);
+            query::Query* query = new query::Query(deleteQuery);
+            $$ = query;
+            *ret = query;
+        } |
+    KW_UPDATE IDENTIFIER KW_SET field_sets KW_WHERE logical_expr SEMICOLON {
+        query::Update* update = new query::Update(*$2, $6, $4);
         query::Query* query = new query::Query(update);
         $$ = query;
         *ret = query;
@@ -84,34 +143,161 @@ start:
 field_defs :
     IDENTIFIER type {
         $$ = new std::vector<query::FieldDefinition*>{};
-        $$->push_back(new query::FieldDefinition($1, $2));
+        $$->push_back(new query::FieldDefinition(*$1, $2));
     } |
     IDENTIFIER type COMMA field_defs {
         $$ = $4;
-        $$->push_back(new query::FieldDefinition($1, $2));
+        $$->push_back(new query::FieldDefinition(*$1, $2));
     }
 ;
 
+field_values :
+    value {
+        $$ = new std::vector<query::FieldValue*>{};
+        $$->push_back($1);
+    } |
+    value COMMA field_values {
+        $$ = $3;
+        $$->push_back($1);
+    }
+
+value :
+    NUMBER {
+        $$ = new query::FieldValue($1);
+    } |
+    REAL {
+        $$ = new query::FieldValue($1);
+    } |
+    STRING {
+        $$ = new query::FieldValue(*$1);
+    }
+
+field_sets :
+    field_set {
+        $$ = new std::vector<query::FieldSet*>{};
+        $$->push_back($1);
+    } |
+    field_set COMMA field_sets {
+        $$ = $3;
+        $$->push_back($1);
+    }
+
+field_set : IDENTIFIER ASSIGN expr {
+        $$ = new query::FieldSet(*$1, $3);
+    }
+
 type : KW_CHAR LPAREN NUMBER RPAREN {
-         $$ = new query::DataType(query::CharType{$3});
+         $$ = new query::DataType(new query::CharType{static_cast<unsigned int>($3)});
+       } |
+       KW_VARCHAR LPAREN NUMBER RPAREN {
+         $$ = new query::DataType(new query::VarCharType{static_cast<unsigned int>($3)});
+       } |
+       KW_INT {
+         $$ = new query::DataType(query::BasicType::INT);
+       } |
+       KW_REAL {
+         $$ = new query::DataType(query::BasicType::REAL);
+       } |
+       KW_TEXT {
+         $$ = new query::DataType(query::BasicType::TEXT);
+       } |
+       KW_BOOLEAN {
+         $$ = new query::DataType(query::BasicType::BOOLEAN);
        }
 ;
 
+exprs:
+    expr {
+        $$ = new std::vector<query::Expr*>{};
+        $$->push_back($1);
+    } |
+    expr COMMA exprs {
+        $$ = $3;
+        $$->push_back($1);
+    }
 
-expr
-    : expr PLUS expr {
-        $$ = $1 + $3;
+logical_expr :
+    KW_TRUE {
+        $$ = new query::LogicalExpr(query::LogicalExprType::TRUE);
+    } |
+    KW_FALSE {
+        $$ = new query::LogicalExpr(query::LogicalExprType::FALSE);
+    } |
+    LPAREN logical_expr RPAREN {
+        $$ = new query::LogicalExpr(query::LogicalExprType::LOG_BRACKETS);
+        $$->_rightLogicalExpr = $2;
+    } |
+    expr KW_EQ expr {
+        $$ = new query::LogicalExpr(query::LogicalExprType::EQ);
+        $$->_leftExpr = $1;
+        $$->_rightExpr = $3;
+    } |
+    expr KW_NEQ expr {
+        $$ = new query::LogicalExpr(query::LogicalExprType::NEQ);
+        $$->_leftExpr = $1;
+        $$->_rightExpr = $3;
+    } |
+    logical_expr KW_OR logical_expr {
+        $$ = new query::LogicalExpr(query::LogicalExprType::OR);
+        $$->_leftLogicalExpr = $1;
+        $$->_rightLogicalExpr = $3;
+    } |
+    logical_expr KW_AND logical_expr {
+        $$ = new query::LogicalExpr(query::LogicalExprType::AND);
+        $$->_leftLogicalExpr = $1;
+        $$->_rightLogicalExpr = $3;
+    } |
+    KW_NOT logical_expr {
+        $$ = new query::LogicalExpr(query::LogicalExprType::NOT);
+        $$->_rightLogicalExpr = $2;
     }
-    | expr MINUS expr {
-        $$ = $1 - $3;
+
+expr:
+    expr PLUS expr {
+        $$ = new query::Expr(query::ExprType::PLUS);
+        $$->_leftExpr = $1;
+        $$->_rightExpr = $3;
+    } |
+    MINUS expr %prec TIMES {
+        $$ = new query::Expr(query::ExprType::UN_MINUS);
+        $$->_rightExpr = $2;
+    } |
+    expr MINUS expr {
+        $$ = new query::Expr(query::ExprType::MINUS);
+        $$->_leftExpr = $1;
+        $$->_rightExpr = $3;
+    } |
+    expr TIMES expr {
+        $$ = new query::Expr(query::ExprType::MULTIPLY);
+        $$->_leftExpr = $1;
+        $$->_rightExpr = $3;
+    } |
+    expr DIVIDE expr {
+        $$ = new query::Expr(query::ExprType::DIVIDE);
+        $$->_leftExpr = $1;
+        $$->_rightExpr = $3;
+    } |
+    LPAREN expr RPAREN {
+        $$ = new query::Expr(query::ExprType::BRACKETS);
+        $$->_rightExpr = $2;
+    } |
+    NUMBER {
+        $$ = new query::Expr(query::ExprType::INTEGER);
+        $$->_integerField = $1;
+    } |
+    REAL {
+        $$ = new query::Expr(query::ExprType::EXPR_REAL);
+        $$->_floatField = $1;
+    } |
+    STRING {
+        $$ = new query::Expr(query::ExprType::STRING);
+        $$->_stringField = *$1;
+    } |
+    IDENTIFIER {
+        $$ = new query::Expr(query::ExprType::FIELD);
+        $$->_stringField = *$1;
+    } |
+    logical_expr {
+        $$ = new query::Expr(query::ExprType::LOGICAL_EXPR);
+        $$->_logicalExpr = $1;
     }
-    | expr TIMES expr {
-        $$ = $1 * $3;
-    }
-    | LPAREN expr RPAREN {
-        $$ = $2;
-    }
-    | NUMBER {
-        $$ = $1;
-    }
-;
--- src/query.h
+++ src/query.h
@@ -3,34 +3,177 @@
 #include <string>
 #include <variant>
 #include <vector>
+#include <iostream>
 
 namespace query {
+
+    class Expr;
+    class LogicalExpr;
+
     class CharType {
     private:
         unsigned int _size;
 
     public:
         CharType(unsigned int size): _size(size) {}
+
+        unsigned int size() {
+            return _size;
+        }
     };
+
+    class VarCharType {
+    private:
+        unsigned int _size;
+
+    public:
+        VarCharType(unsigned int size): _size(size) {}
+
+        unsigned int size() {
+            return _size;
+        }
+    };
+
+    enum BasicType { INT, REAL, TEXT, BOOLEAN, UNKNOWN_TYPE };
 
     class DataType {
     private:
-        std::variant<CharType> type;
+        std::variant<CharType*, VarCharType*, BasicType> type;
 
     public:
-        DataType(CharType charType): type(charType) {
+        DataType(CharType* charType): type(charType) {}
+        DataType(VarCharType* varCharType): type(varCharType) {}
+        DataType(BasicType basicType): type(basicType) {}
+
+        CharType* getAsChar() {
+            if (!std::holds_alternative<CharType*>(type))
+                return nullptr;
+            return std::get<CharType*>(type);
+        }
+
+        VarCharType* getAsVarChar() {
+            if (!std::holds_alternative<VarCharType*>(type))
+                return nullptr;
+            return std::get<VarCharType*>(type);
+        }
+
+        BasicType getAsBasic() {
+            if (!std::holds_alternative<BasicType>(type))
+                return BasicType::UNKNOWN_TYPE;
+            return std::get<BasicType>(type);
         }
     };
 
 
     class FieldDefinition {
     private:
-        std::string *_name;
-        DataType *_type;
+        std::string _name;
+        DataType* _type;
 
     public:
-        FieldDefinition(std::string *name, DataType* type): _name(name), _type(type) {
+        FieldDefinition(std::string name, DataType *type):
+            _name(name), _type(type) {}
+
+        std::string name() {
+            return _name;
         }
+
+        std::string type() {
+            if (_type->getAsChar() != nullptr)
+                return "CHAR[" + std::to_string(_type->getAsChar()->size()) + "]";
+            if (_type->getAsVarChar() != nullptr)
+                return "VARCHAR[" + std::to_string(_type->getAsVarChar()->size()) + "]";
+
+
+            BasicType basic = _type->getAsBasic();
+            switch (basic) {
+                case BasicType::BOOLEAN:
+                    return "BOOLEAN";
+                case BasicType::INT:
+                    return "INT";
+                case BasicType::REAL:
+                    return "REAL";
+                case BasicType::TEXT:
+                    return "TEXT";
+                default:
+                    return "UNKNOWN";
+            }
+        }
+    };
+
+    class FieldValue {
+    private:
+        std::variant<int, float, std::string, bool> _value;
+
+    public:
+        FieldValue(int value) : _value(value) {}
+        FieldValue(float value) : _value(value) {}
+        FieldValue(std::string value) : _value(value) {}
+        FieldValue(bool value) : _value(value) {}
+
+        std::string getString() {
+            if (std::holds_alternative<int>(_value))
+                return "INT: " + std::to_string(std::get<int>(_value));
+            else if (std::holds_alternative<float>(_value))
+                return "REAL: " + std::to_string(std::get<float>(_value));
+            else if (std::holds_alternative<std::string>(_value))
+                return "TEXT: " + std::get<std::string>(_value) + "";
+            else if (std::holds_alternative<bool>(_value)) {
+                if (std::get<bool>(_value))
+                    return "BOOLEAN TRUE";
+                return "BOOLEAN FALSE";
+            }
+            return "UNKNOWN";
+        }
+    };
+
+    class FieldSet {
+    private:
+        std::string _name;
+        Expr* _expr;
+
+    public:
+        FieldSet(std::string name, Expr* expr) : _name(name), _expr(expr) {}
+
+        std::string name() {
+            return _name;
+        }
+
+        Expr* expr() {
+            return _expr;
+        }
+    };
+
+    enum ExprType { PLUS, MINUS, UN_MINUS, MULTIPLY, DIVIDE, BRACKETS,
+            INTEGER, EXPR_REAL, STRING, FIELD, LOGICAL_EXPR };
+    enum LogicalExprType { TRUE, FALSE, LOG_BRACKETS, EQ, NEQ, AND, OR, NOT };
+
+    class Expr {
+    public:
+        ExprType _type;
+        int _integerField;
+        std::string _stringField;
+        float _floatField;
+        Expr* _leftExpr = nullptr;
+        Expr* _rightExpr = nullptr;
+        LogicalExpr* _logicalExpr = nullptr;
+
+        Expr(ExprType type) : _type(type) {}
+
+        std::string print(int depth);
+    };
+
+    class LogicalExpr {
+    public:
+        LogicalExprType _type;
+        Expr* _leftExpr = nullptr;
+        Expr* _rightExpr = nullptr;
+        LogicalExpr* _leftLogicalExpr = nullptr;
+        LogicalExpr* _rightLogicalExpr = nullptr;
+
+        LogicalExpr(LogicalExprType type) : _type(type) {}
+
+        std::string print(int depth);
     };
 
     enum Type {CREATE, INSERT, SELECT, DELETE, UPDATE, UNKNOWN};
@@ -41,61 +184,101 @@ namespace query {
     class CreateTable : Query2 {
     private:
         std::string _name;
-        std::vector<FieldDefinition*>* _fieldDefs;
+        std::vector<FieldDefinition*>* _fieldDefs = nullptr;
     public:
-        CreateTable(std::string name, std::vector<FieldDefinition*>* fieldDefs): _name(name), _fieldDefs(fieldDefs) {
-        }
+        CreateTable(std::string name, std::vector<FieldDefinition*>* fieldDefs):
+            _name(name), _fieldDefs(fieldDefs) {}
 
         std::string name() {
             return _name;
+        }
+
+        std::vector<FieldDefinition*>* fieldDefs() {
+            return _fieldDefs;
         }
     };
 
     class Insert {
     private:
         std::string _name;
+        std::vector<FieldValue*>* _field_values = nullptr;
     public:
-        Insert(std::string name): _name(name) {
-        }
+        Insert(std::string name, std::vector<FieldValue*>* values):
+            _name(name), _field_values(values) {}
 
         std::string name() {
             return _name;
+        }
+
+        std::vector<FieldValue*>* values() {
+            return _field_values;
         }
     };
 
     class Delete {
     private:
         std::string _name;
+        LogicalExpr* _logical = nullptr;
     public:
-        Delete(std::string name): _name(name) {
-        }
+        Delete(std::string name):
+            _name(name) {}
+        Delete(std::string name, LogicalExpr* logical):
+            _name(name), _logical(logical) {}
 
         std::string name() {
             return _name;
+        }
+
+        LogicalExpr* logical() {
+            return _logical;
         }
     };
 
     class Update {
     private:
         std::string _name;
+        LogicalExpr* _logical = nullptr;
+        std::vector<FieldSet*>* _sets = nullptr;
     public:
-        Update(std::string name): _name(name) {
-        }
+        Update(std::string name, LogicalExpr* logical, std::vector<FieldSet*>* sets):
+            _name(name), _logical(logical), _sets(sets) {}
 
         std::string name() {
             return _name;
+        }
+
+        std::vector<FieldSet*>* sets() {
+            return _sets;
+        }
+
+        LogicalExpr* logical() {
+            return _logical;
         }
     };
 
     class Select {
     private:
         std::string _name;
+        LogicalExpr* _logical = nullptr;
+        std::vector<Expr*>* _exprs = nullptr;
     public:
-        Select(std::string name): _name(name) {
-        }
+        Select(std::string name):
+            _name(name) {}
+        Select(std::string name, LogicalExpr* logical):
+            _name(name), _logical(logical) {}
+        Select(std::string name, LogicalExpr* logical, std::vector<Expr*>* exprs):
+            _name(name), _logical(logical), _exprs(exprs) {}
 
         std::string name() {
             return _name;
+        }
+
+        std::vector<Expr*>* exprs() {
+            return _exprs;
+        }
+
+        LogicalExpr* logical() {
+            return _logical;
         }
 
     };
@@ -103,6 +286,7 @@ namespace query {
     class Query {
     private:
         std::variant<CreateTable*, Insert*, Delete*, Update*, Select*> query;
+        int depth = 2;
     public:
         Query() {
         }
@@ -159,6 +343,77 @@ namespace query {
         }
 
         void debug_print() {
+            std::cout << "Query: ";
+            switch (this->type())
+            {
+                case query::Type::CREATE: {
+                    query::CreateTable* query = this->createTable();
+
+                    std::cout << "CREATE" << std::endl << "Table: " << query->name() << std::endl
+                        << "Field definitions:" << std::endl;
+                    std::vector<query::FieldDefinition*>* fieldDefs = query->fieldDefs();
+                    for (query::FieldDefinition* fieldDef : *fieldDefs)
+                        std::cout << std::string(depth, ' ') << fieldDef->type() << ": "
+                            << fieldDef->name() << std::endl;
+                    break;
+                }
+                case query::Type::UPDATE: {
+                    query::Update* query = this->update();
+
+                    std::cout << "UPDATE" << std::endl << "Table: " << query->name() << std::endl;
+                    std::vector<query::FieldSet*>* sets = query->sets();
+                    for (query::FieldSet* set : *sets)
+                        std::cout << std::string(depth, ' ') << set->name() << ":"
+                            << std::endl << set->expr()->print(depth + 2) << std::flush;
+
+                    std::cout << "Where:" << std::endl << query->logical()->print(depth) << std::flush;
+                    break;
+                }
+                case query::Type::DELETE: {
+                    query::Delete* query = this->deleteQuery();
+
+                    std::cout << "DELETE" << std::endl << "Table: " << query->name() << std::endl;
+
+                    query::LogicalExpr* logical = query->logical();
+                    if (logical != nullptr)
+                        std::cout << "Where:" << std::endl << logical->print(depth) << std::flush;
+                    break;
+                }
+                case query::Type::INSERT: {
+                    query::Insert* query = this->insert();
+
+                    std::cout << "INSERT" << std::endl << "Table: " << query->name() << std::endl
+                        << "Values:" << std::endl;
+                    std::vector<query::FieldValue*>* values = query->values();
+                    for (query::FieldValue* value : *values)
+                        std::cout << std::string(depth, ' ') << value->getString() << std::endl;
+                    break;
+                }
+                case query::Type::SELECT: {
+                    query::Select* query = this->select();
+
+                    std::cout << "SELECT" << std::endl << "Table: " << query->name() << std::endl << "What:";
+                    std::vector<query::Expr*>* exprs = query->exprs();
+                    if (exprs == nullptr)
+                        std::cout << " *" << std::endl;
+                    else {
+                        std::cout << std::endl;
+                        for (query::Expr* expr : *exprs)
+                            std::cout << expr->print(depth) << std::flush;
+                    }
+
+                    query::LogicalExpr* logical = query->logical();
+                    if (logical != nullptr)
+                        std::cout << "Where:" << std::endl << logical->print(depth) << std::flush;
+                    break;
+                }
+                case query::Type::UNKNOWN: {
+                    std::cout << "Unknown command" << std::endl;
+                    break;
+                }
+                default:
+                    break;
+            }
         }
     };
 
